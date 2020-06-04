0257c955b997e09a625448a29cce538931780ec4
AnnaGrace <anna.grace.conover@gmail.com>
2020-06-04 16:11:26 -0500

    push issues

---
+++ public/js/homepage.js
@@ -0,0 +1,84 @@
+$("#addtask").on("click", function(){
+
+
+})
+
+$("#seehistory").on("click", function(){
+
+    
+})
+
+$("#sleeporwakeup").on("click", function(){
+
+    
+})
+
+$("#complete").on("click", function(){
+
+    
+})
+
+$("#update").on("click", function(){
+
+    
+})
+
+$("#delete").on("click", function(){
+
+    
+})
+
+$("#logout").on("click", function(){
+
+    
+})
+
+$("#switchuser").on("click", function(){
+
+    
+})
+
+$("#savenewtask").on("click", function(){
+
+    
+})
+
+$("#readytostartyes").on("click", function(){
+
+    
+})
+
+$("#readytostartno").on("click", function(){
+
+    
+})
+
+$("#enddayyes").on("click", function(){
+
+    
+})
+
+$("#enddayno").on("click", function(){
+
+    
+})
+
+$("#login").on("click", function(){
+
+    
+})
+
+$("#signup").on("click", function(){
+
+    
+})
+
+$("#sidekickcat").on("click", function(){
+
+    
+})
+
+$("#sidekickdog").on("click", function(){
+
+    
+})
