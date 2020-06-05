f1dc06faab94cb371c0896ac048e4c7d95097fe9
AnnaGrace <anna.grace.conover@gmail.com>
2020-06-04 20:56:22 -0500

    Made the two images radio choices. Hid the the circle buttons and made the images the buttons

--- models/user.js
+++ models/user.js
@@ -23,13 +23,11 @@ module.exports = function(sequelize, DataTypes) {
     User.hasMany(models.Activity, {
       onDelete: "cascade"
     });
-  };
-
-  User.associate = function(models) {
     User.hasMany(models.Sidekick, {
       onDelete: "cascade"
     })
-  }
+  };
+
 
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
   User.prototype.validPassword = function(password) {
