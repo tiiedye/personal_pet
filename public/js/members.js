044a0dd2a6696073eda1df80eedda167bc78a804
Sharlene May <sharleneminosa@gmail.com>
2020-06-08 11:31:08 -0700

    exit from activity modal

--- public/js/members.js
+++ public/js/members.js
@@ -1,90 +1,93 @@
 $(document).ready(() => {
   var thisId;
 
+  console.log("member's script is loaded");
 
-    console.log("member's script is loaded");
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then((data) => {
     $(".member-name").text(data.email);
   });
 
-        
-        $(".complete").on("click", function(event) {
-            event.preventDefault();
-            var happinessValue = $(this).attr("data-value");
-            var taskId = $(this).attr("data-id");
-            $.get("/api/sidekick", function(data) {
-                var happinessPnts = parseInt(happinessValue) + parseInt(data[0].Sidekicks[0].happinessPoints);
-                $.ajax({
-                    type: "PUT",
-                    url: "/api/sidekick",
-                    data: { 'id': taskId, 'happinessPnts': happinessPnts }
-                }).then(function() {
-                    updateImg();
-                    updateProgress();
-                });
-            });
+  $(".complete").on("click", function(event) {
+    event.preventDefault();
+
+    $.get("/api/sidekick", function(data) {
+      // happinessPoints = 0
+      // happinessValue = 4
+      // table happinessPoints = 0
+      var happinessValue = $(this.value);
+      var happinessPnts =
+        happinessValue + parseInt(data[0].Sidekicks[0].happinessPoints);
+
+      $.ajax({
+        type: "PUT",
+        url: "/api/sidekick",
+        data: { "Sidekicks.happinessPoints": happinessPnts },
+      }).then(function() {
+        updateImage();
+        updateProgress();
       });
+    });
+  });
 
-    function updateProgress() {
-        $.get("/api/sidekick/", function(data) {
-            $(".progressBar").attr("value", data[0].Sidekicks[0].happinessPoints);
+  function updateProgress() {
+    $.get("/api/sidekick/", function(data) {
+      $(".progressBar").attr("value", data[0].Sidekicks[0].happinessPoints);
+    });
+  }
 
-        })
-    
-    };
+  //   test jQuery to be deleted later
+  $(".update-activity").on("click", function() {
+    updateProgress();
+  });
 
-
-    function updateImg() {
-        $.get("/api/sidekick", function(data) {
-          console.log(data);
-    
-            //gets userId so that activities can be user specific
-            thisId = data[0].Sidekicks[0].UserId
-    
-            if (data[0].Sidekicks[0].sidekickImage === "dog") {
-                if (data[0].Sidekicks[0].happinessPoints < 30) {
-                    $(".imgDiv").empty();
-                    $(".imgDiv").html("<img src='assets/Dog-Alert.png'>");
-                } else if (data[0].Sidekicks[0].happinessPoints > 60) {
-                    $(".imgDiv").empty();
-                    $(".imgDiv").html("<img src='assets/Dog-Happy.png'>");
-                } else {
-                    $(".imgDiv").empty();
-                    $(".imgDiv").html("<img src='assets/Dog-Neutral.png'>");
-                }
-            } else if (data[0].Sidekicks[0].sidekickImage === "cat") {
-                if (data[0].Sidekicks[0].happinessPoints < 30) {
-                    $(".imgDiv").empty();
-                    $(".imgDiv").html("<img src='assets/Cat-Alert.png'>");
-                } else if (data[0].Sidekicks[0].happinessPoints > 60) {
-                    $(".imgDiv").empty();
-                    $(".imgDiv").html("<img src='assets/Cat-Happy.png'>");
-                } else {
-                    $(".imgDiv").empty();
-                    $(".imgDiv").html("<img src='assets/Cat-Neutral.png'>");
-                }
-            }
-        });
-    }
+  function updateImg() {
+    $.get("/api/sidekick", function(data) {
+      console.log(data);
+      thisId = data[0].Sidekicks[0].UserId;
+      if (data[0].Sidekicks[0].sidekickImage === "dog") {
+        if (data[0].Sidekicks[0].happinessPoints < 30) {
+          $(".imgDiv").empty();
+          $(".imgDiv").html("<img src='assets/Dog-Alert.png'>");
+        } else if (data[0].Sidekicks[0].happinessPoints > 60) {
+          $(".imgDiv").empty();
+          $(".imgDiv").html("<img src='assets/Dog-Happy.png'>");
+        } else {
+          $(".imgDiv").empty();
+          $(".imgDiv").html("<img src='assets/Dog-Neutral.png'>");
+        }
+      } else if (data[0].Sidekicks[0].sidekickImage === "cat") {
+        if (data[0].Sidekicks[0].happinessPoints < 30) {
+          $(".imgDiv").empty();
+          $(".imgDiv").html("<img src='assets/Cat-Alert.png'>");
+        } else if (data[0].Sidekicks[0].happinessPoints > 60) {
+          $(".imgDiv").empty();
+          $(".imgDiv").html("<img src='assets/Cat-Happy.png'>");
+        } else {
+          $(".imgDiv").empty();
+          $(".imgDiv").html("<img src='assets/Cat-Neutral.png'>");
+        }
+      }
+    });
+  }
 
   updateImg();
 
-  let activities = [];
-  let category = $("#selectCategory option:selected").text();
+  // ACTIVITIES MODAL
+
   // let difficulty = $(".form-check-input:checked").val();
   const activityname = $("#addActivity");
-
+  
   $("#savenewtask").on("click", function(event) {
-    event.preventDefault();
+      let category = $("#selectCategory option:selected").text();
+      event.preventDefault();
     const activityData = {
       activityName: activityname.val().trim(),
       priority: $(".form-check-input:checked").val(),
       category: category,
     };
-
-    console.log("********", activityData);
+    // console.log("********", activityData);
 
     createActivity(activityData);
   });
@@ -98,18 +101,7 @@ $(document).ready(() => {
       console.log("post was successful!", data);
     });
 
-
-    //$.post("/api/activity", function(data){
-    //    alert("success");
-    //}).then(function () {
-    //    console.log("new activity added: " + activityData);
-    //});
   }
-
-        $(".addEmailForm").on("click", function (event) {
-            event.preventDefault();
-            console.log("party?")
-        });
 });
 
 
