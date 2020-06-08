037a7f8173b4b058d9330e831e2b71b60230c5eb
Julia Clements <tiiedye@gmail.com>
2020-06-08 10:53:28 -0600

    getting complete to work

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,5 +1,6 @@
 // Requiring path to so we can use relative routes to our HTML files
 const path = require("path");
+var db = require("../models");
 
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
@@ -24,6 +25,11 @@ module.exports = function(app) {
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
-    res.render("members")
+    db.User.findAll({
+      include: [db.Sidekick]
+  }).then(function(sidekickGetResults) {
+    console.log(sidekickGetResults[0].dataValues);
+    res.render("members", sidekickGetResults[0].dataValues);
+  })
   });
 };
