7f2b0c0e2659eafc27da34d9aa94674ee4985402
Sharlene May <sharleneminosa@gmail.com>
2020-06-09 12:10:57 -0700

    activities save and are rendered to page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -31,27 +31,29 @@ module.exports = function(app) {
       },
       include: [db.Sidekick, db.Activity],
     }).then(function(sidekickGetResults) {
-      console.log(sidekickGetResults);
+      console.log("**************sidekickGetResults[0].dataValues.Activities******************")
+      console.log(sidekickGetResults[0].dataValues.Activities);
       // console.log("sidekickGetResults[0].dataValues.Activities[0].activityName************");
       // console.log(sidekickGetResults[0].dataValues.Activities.activityName);
       // console.log("This underneath is .Activities.dataValues", sidekickGetResults[0].Activities.dataValues);
-      let activity;
+      let activityObject;
       // console.log(sidekickGetResults[0].Activities[0].dataValues)
       if (sidekickGetResults[0].Activities[0] === undefined) {
         console.log("there are no activities");
-        activity = 0;
+        activityObject = 0;
       } else {
         console.log("********this***********")
-        console.log(sidekickGetResults[0].dataValues.Activities[0].activityName);
-        activity = [{
-          activityName: sidekickGetResults[0].dataValues.Activities[0].activityName
-        }];
-        console.log("activies has stuff");
+        // console.log(sidekickGetResults[0].dataValues.Activities[0].activityName);
+        activityObject = {
+          test: sidekickGetResults[0].dataValues.Activities
+        }
+        console.log("activities has stuff");
       }
-      console.log(activity);
+      console.log("**********activityObject************")
+      console.log(activityObject);
       res.render("members", {
         sidekick: sidekickGetResults[0].dataValues,
-        activity,
+        activityObject,
       });
     });
 
@@ -66,3 +68,15 @@ module.exports = function(app) {
 //   res.render("members", sidekickGetResults[0].dataValues);
 // })
 // });
+
+// else {
+//   console.log("********this***********")
+//   console.log(sidekickGetResults[0].dataValues.Activities[0].activityName);
+//   for (i = 0; i < sidekickGetResults.length; i++){
+//     activity = {
+//       activityName: sidekickGetResults[i].dataValues.Activities[i].activityName
+//     };
+//   }
+//   console.log("activities has stuff");
+//   console.log(activity);
+// }
